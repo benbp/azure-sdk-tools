2cd0f84f738db29c54bc188e020b4aa1464c626d
Ben Broderick Phillips <bebroder@microsoft.com>
2021-08-25 19:44:49 -0400

    [stress testing] Add shared informer example

---
+++ tools/stress-cluster/services/cmd/pod_event_handler/main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/util/runtime"
+
+	"k8s.io/client-go/informers"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/cache"
+	"k8s.io/client-go/tools/clientcmd"
+)
+
+func main() {
+	log.Print("Shared Informer app started")
+	kubeconfig := os.Getenv("KUBECONFIG")
+	config, err := clientcmd.BuildConfigFromFlags("", kubeconfig)
+	if err != nil {
+		log.Panic(err.Error())
+	}
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		log.Panic(err.Error())
+	}
+
+	factory := informers.NewSharedInformerFactory(clientset, 0)
+	informer := factory.Core().V1().Pods().Informer()
+	stopper := make(chan struct{})
+	defer close(stopper)
+	defer runtime.HandleCrash()
+	informer.AddEventHandler(cache.ResourceEventHandlerFuncs{
+		AddFunc: onAdd,
+	})
+	go informer.Run(stopper)
+	if !cache.WaitForCacheSync(stopper, informer.HasSynced) {
+		runtime.HandleError(fmt.Errorf("Timed out waiting for caches to sync"))
+		return
+	}
+	<-stopper
+}
+
+// onAdd is the function executed when the kubernetes informer notified the
+// presence of a new kubernetes node in the cluster
+func onAdd(obj interface{}) {
+	// Cast the obj as node
+	pod := obj.(*corev1.Pod)
+	_, ok := pod.GetLabels()["foo"]
+	if ok {
+		fmt.Printf("It has the label!")
+	}
+}
